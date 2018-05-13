4885de6b317372a9dfaeae64aa9cb8724d32919e
coderpro1123211 <osktor0325@utb.partille.se>
2018-05-13 20:03:18 +0200

    Added all the dependencies and fixed build

--- Dusk Engine - Core/Main.cpp
+++ Dusk Engine - Core/Main.cpp
@@ -1,5 +1,18 @@
-#include "Window.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "src/core/Window.h"
 
-int main(int argc, char* argv[]) {
+int main() {
+	
+	if (!glfwInit())
+	{
+		std::cout << "GLFW Init failed" << std::endl;
+		system("PAUSE");
+		return 1;
+	}
+
+	Dusk::Core::Window* w = new Dusk::Core::Window(800, 600);
+
+	system("PAUSE");
 	return 0;
 }
--- Dusk Engine - Core/src/core/Window.cpp
+++ Dusk Engine - Core/src/core/Window.cpp
@@ -1,7 +1,9 @@
 #include "Window.h"
+#include "GLFW/glfw3.h"
 
 Dusk::Core::Window::Window(int w, int h)
 {
 	m_Width = Width = w;
 	m_Height = Height = h;
+	glfwCreateWindow(w, h, "Hello World!", NULL, NULL);
 }
